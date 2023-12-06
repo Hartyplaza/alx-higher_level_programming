cb499510366e9322e285299c66caad39b639daf2
Hartyplaza <ofigwehart@gmail.com>
2023-12-05 18:28:56 -0800

    inheritance

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" A class """
+
+
+class BaseGeometry:
+    """ Base class """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ A Derive class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ Return the sum of geometry """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """ String representation """
+        return "[{}] {}/{}".format(
+                self.__class__.__name__, self.__width, self.__height)
